3c74d9a96054acf7233e5d69729055b06ce5986d
Fedorek Dávid <fedorekdavid@gmail.com>
2025-11-28 22:32:45 +0100

    Improve animation handling

--- prague-jam/Player.cs
+++ prague-jam/Player.cs
@@ -12,16 +12,21 @@ public partial class Player : Area2D
 		Position = position;
 	}
 	
+	private AnimatedSprite2D _animatedSprite2D;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
+		_animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_animatedSprite2D.Play("idle");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		Vector2 velocity = Vector2.Zero;
+		Vector2 scale = new Vector2(1, 1); 
 
 		if (Input.IsActionPressed("move_right"))
 		{
@@ -31,6 +36,8 @@ public partial class Player : Area2D
 		if (Input.IsActionPressed("move_left"))
 		{
 			velocity.X -= 1;
+			scale.X = -1;
+
 		}
 
 		if (Input.IsActionPressed("move_down"))
@@ -43,8 +50,26 @@ public partial class Player : Area2D
 			velocity.Y -= 1;
 		}
 		
+		if (Input.IsActionPressed("attack"))
+		{
+			changeToAttack("idle");
+		}
+		
 		if (velocity.Length() > 0){
 			velocity = velocity.Normalized()*Speed;
+		}
+
+
+		if (!(_animatedSprite2D.GetAnimation() == "attack"))
+		{
+			if (velocity != Vector2.Zero)
+			{
+				_animatedSprite2D.Play("run");
+			}
+			else
+			{
+				_animatedSprite2D.Play("idle");
+			}
 		}
 
 		Position += velocity * (float)delta;
@@ -52,5 +77,16 @@ public partial class Player : Area2D
 			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
 			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
 		);
+
+		Scale = scale;
+	}
+
+	void changeToAttack(String next)
+	{
+		_animatedSprite2D.Play("attack");
+		_animatedSprite2D.AnimationFinished += () =>
+		{
+			_animatedSprite2D.Play(next);
+		};
 	}
 }
