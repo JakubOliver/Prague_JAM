6ed6befb4bc3bd8f4f42dc89b80f0a47e16fe16a
Fedorek Dávid <fedorekdavid@gmail.com>
2025-11-28 21:58:49 +0100

    Add support for Idle animation of Soldier

--- prague-jam/Soldier.cs
+++ prague-jam/Soldier.cs
@@ -5,15 +5,19 @@ public partial class Soldier : Area2D
 {
 	[Signal]
 	public delegate void HitEventHandler();
+
+	private AnimatedSprite2D _animatedSprite2D;
 	
 	private void OnBodyEntered(Node2D body) {
+		
 		GD.Print("třeba ahoj");
 	}
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		_animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_animatedSprite2D.Play("idle");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
