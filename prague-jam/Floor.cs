7c7f167787dca6e5d77553806f9c776916fb1321
Fedorek Dávid <fedorekdavid@gmail.com>
2025-11-28 20:50:23 +0100

    Update

---
+++ prague-jam/Floor.cs
@@ -0,0 +1,6 @@
+using Godot;
+using System;
+
+public partial class Floor : Area2D
+{
+}
--- prague-jam/Soldier.cs
+++ prague-jam/Soldier.cs
@@ -1,52 +1,62 @@
-using System;
 using Godot;
 
-public partial class Soldier : Area2D, IPerson, IMove
+public partial class Soldier : RigidBody2D, IPerson, IMove
 {
-    public int HP { get; set; } = 150;
-    public int Damage { get; set; } = 20;
-
-    private Random _rng = new Random();
-    
-    [Export]	
-    public int Speed { get; set; } = 400000;
-    public Vector2 ScreenSize;
-
-    public PersonClothing head { get; set; } = PersonClothing.Soldier;
-    public PersonClothing body { get; set; } = PersonClothing.Soldier;
-    public PersonClothing weapon { get; set; } = PersonClothing.Soldier;
-
-    public void Move(double delta)
-    {
-        Vector2 velocity = Vector2.Zero;
-        int dir1 = _rng.Next(0, 2);
-        int dir2 = _rng.Next(0, 2);
-        velocity.X = dir1 == 1 ? Speed : -Speed;
-        velocity.Y = dir2 == 1 ? Speed : -Speed;
-        
-        Position += velocity * (float)delta;
-        Position = new Vector2(
-            x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-            y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-        );
-    }
-    
-    public void Start(Vector2 position)
-    {
-        Position = position;
-    }
+	[Signal]
+	public delegate void HitEventHandler();
 	
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        ScreenSize = GetViewportRect().Size;
-    }
+	public int HP { get; set; } = 150;
+	public int Damage { get; set; } = 20;
+	
+	[Export]	
+	public int Speed { get; set; } = 400;
+	public Vector2 ScreenSize;
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        Move(delta);
+	public PersonClothing head { get; set; } = PersonClothing.Soldier;
+	public PersonClothing body { get; set; } = PersonClothing.Soldier;
+	public PersonClothing weapon { get; set; } = PersonClothing.Soldier;
 
-        GD.Print(Position);
-    }
+	public void Move(double delta)
+	{
+		Vector2 Velocity = new Vector2(100, 100);
+		
+		// Using MoveAndCollide.
+		var collision = MoveAndCollide(Velocity * (float)delta);
+		if (collision != null)
+		{
+			GD.Print("I collided with ", ((Node)collision.GetCollider()).Name);
+		}
+	}
+	
+	private void OnBodyEntered(Node2D node){
+		GD.Print("ahoj");
+	}
+	
+	public void Start(Vector2 position)
+	{
+		Position = position;
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ContactMonitor = true;
+		MaxContactsReported = 25;
+		ScreenSize = GetViewportRect().Size;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		//Move(delta);
+
+		//GD.Print(Position);
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+		
+		Move(delta);
+	}
 }
