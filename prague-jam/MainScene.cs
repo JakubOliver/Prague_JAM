b1d748abe577741421ac2a2374c2ceef9839c58f
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2025-11-29 13:48:40 +0100

    Update

--- prague-jam/MainScene.cs
+++ prague-jam/MainScene.cs
@@ -6,12 +6,13 @@ public partial class MainScene : Node2D
 	private AnimationPlayer AnimationPlayer;
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public override async void _Ready()
 	{
 		AnimationPlayer = GetNode<AnimationPlayer>("Transition/AnimationPlayer");
 		//AnimationPlayer.get_parent().get_node("ColorRect")1.modulate = new Color(255, 255, 255, 1);
 		AnimationPlayer.GetParent().GetNode<ColorRect>("ColorRect").Modulate = new Color(0, 0, 0, 0);
 		AnimationPlayer.Play("fade_out");
+		await ToSignal(AnimationPlayer, "animation_finished");
 	}
 
 	public override async void _Input(InputEvent @event)
