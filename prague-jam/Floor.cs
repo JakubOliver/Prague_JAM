b54c8983a8981d0880ff37d06b12b062001c7320
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2025-11-29 14:33:44 +0100

    Update Floor

---
+++ prague-jam/Floor.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Floor : TileMapLayer
+{
+	const int _maxNotOutOfRangeCoordinateX = 113;
+	const int _maxNotOutOfRangeCoordinateY = 38;
+	
+	private readonly Queue<Vector2I> _lavaTiles = new();
+
+	private void SetLavaTile(Vector2I coordinates)
+	{
+		_lavaTiles.Enqueue(coordinates);
+		SetCell(coordinates, 0, new Vector2I(0, 0));
+	}
+
+	public void GenerateRandomLavaTile()
+	{
+		int x = GD.RandRange(0, _maxNotOutOfRangeCoordinateX);
+		int y = GD.RandRange(0, _maxNotOutOfRangeCoordinateY);
+		
+		GD.Print(x);
+		GD.Print(y);
+		SetLavaTile(new Vector2I(x, y));
+	}
+	public void ClearLeastRecentLavaTile()
+	{
+		if (_lavaTiles.TryDequeue(out var coordinates))
+			EraseCell(coordinates);
+	}
+	
+	public void CoverWithLava() {
+		for (int x = 0; x <= _maxNotOutOfRangeCoordinateX; ++x)
+		for (int y = 0; y <= _maxNotOutOfRangeCoordinateY; ++y)
+			SetLavaTile(new Vector2I(x, y));
+	}	
+	
+	public override void _Ready()
+	{
+		GD.Seed(12345);
+		for (int i = 0; i < 10; ++i) GenerateRandomLavaTile();
+		for (int i = 0; i < 5; ++i) ClearLeastRecentLavaTile();
+	}
+}
